8b6f89246a8d535519818d42d3a12b1902d18b72
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-09 03:31:27 +0400

    Update EditionBindingModel.cs

--- PrinterySVC/BindingModel/EditionBindingModel.cs
+++ PrinterySVC/BindingModel/EditionBindingModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PrinterySVC.BindingModel
 {
-    public class EdiitionBindingModel
+    public class EditionBindingModel
     {
         public int Number { get; set; }
 
