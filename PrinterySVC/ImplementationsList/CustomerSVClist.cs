7398976a98ac1192ebc5ecddd8acb5d4ab1a2482
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-10 23:11:50 +0400

    Update CustomerSVClist.cs

--- PrinterySVC/ImplementationsList/CustomerSVClist.cs
+++ PrinterySVC/ImplementationsList/CustomerSVClist.cs
@@ -27,18 +27,24 @@ namespace PrinterySVC.ImplementationsList
 
         public void UpElement(CustomerBindingModel model)
         {
-            Customer element = source.Customers.FirstOrDefault(rec =>
-                                    rec.CustomerFIO == model.CustomerFIO && rec.Number != model.Number);
-            if (element != null)
+                        int index = -1;
+            for (int i = 0; i < source.Customers.Count; ++i)
             {
-                throw new Exception("Уже есть клиент с таким ФИО");
+                if (source.Customers[i].Number == model.Number)
+                {
+                    index = i;
+                }
+                if (source.Customers[i].CustomerFIO == model.CustomerFIO &&
+                    source.Customers[i].Number != model.Number)
+                {
+                    throw new Exception("Уже есть клиент с таким ФИО");
+                }
             }
-            element = source.Customers.FirstOrDefault(rec => rec.Number == model.Number);
-            if (element == null)
+            if (index == -1)
             {
                 throw new Exception("Элемент не найден");
             }
-            element.CustomerFIO = model.CustomerFIO;
+            source.Customers[index].CustomerFIO = model.CustomerFIO;
         }
 
         public void DelElement(int id)
