6af15bff3816f45c70f882dc7e6a0fa5ee6bb4d3
Анастасия Мошина <moshinaan@gmail.com>
2018-06-11 00:47:10 +0400

    desklab16

--- PrinterySVC/Inteface/IReportSVC.cs
+++ PrinterySVC/Inteface/IReportSVC.cs
@@ -1,4 +1,5 @@
-﻿using PrinterySVC.BindingModel;
+﻿using PrinterySVC.Attributies;
+using PrinterySVC.BindingModel;
 using PrinterySVC.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,23 @@ using System.Threading.Tasks;
 
 namespace PrinterySVC.Inteface
 {
+
+    [CustomInterface("Интерфейс для работы с отчетами")]
     public interface IReportSVC
     {
+        [CustomMethod("Метод сохранения списка изделий в doc-файл")]
         void SaveProductPrice(ReportBindingModel model);
 
+        [CustomMethod("Метод получения списка складов с количество компонент на них")]
         List<RacksLoadViewModel> GetRacksLoad();
 
+        [CustomMethod("Метод сохранения списка списка складов с количество компонент на них в xls-файл")]
         void SaveRacksLoad(ReportBindingModel model);
 
+        [CustomMethod("Метод получения списка заказов клиентов")]
         List<CustomerBookcingsModel> GetCustomerOrders(ReportBindingModel model);
 
+        [CustomMethod("Метод сохранения списка заказов клиентов в pdf-файл")]
         void SaveCustomerOrders(ReportBindingModel model);
     }
 }
