bcd6538d9e42a91327fb4c02e652bbb8ddf83fc8
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-10 23:08:48 +0400

    Update CustomerSVClist.cs

--- PrinterySVC/ImplementationsList/CustomerSVClist.cs
+++ PrinterySVC/ImplementationsList/CustomerSVClist.cs
@@ -25,15 +25,6 @@ namespace PrinterySVC.ImplementationsList
             throw new Exception("Элемент не найден");
         }
 
-        public void AddElement(CustomerBindingModel model)
-        {
-            source.Customers.Add(new Customer
-            {
-                Number = maxNumber + 1,
-                CustomerFIO = model.CustomerFIO
-            });
-        }
-
         public void UpElement(CustomerBindingModel model)
         {
             Customer element = source.Customers.FirstOrDefault(rec =>
