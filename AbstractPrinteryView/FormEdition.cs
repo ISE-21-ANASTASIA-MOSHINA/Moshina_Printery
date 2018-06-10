b20079041b9b34028f6402f2af1b68486d423d31
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-10 22:45:04 +0400

    Update FormEdition.cs

--- AbstractPrinteryView/FormEdition.cs
+++ AbstractPrinteryView/FormEdition.cs
@@ -192,7 +192,6 @@ namespace AbstractPrinteryView
                 MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
         private void buttonCancel_Click(object sender, EventArgs e)
         {
             DialogResult = DialogResult.Cancel;
