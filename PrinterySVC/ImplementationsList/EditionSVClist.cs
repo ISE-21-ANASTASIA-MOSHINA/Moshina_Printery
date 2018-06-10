6ce051668c743d88aa5ded8d83803265b4599e93
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-11 00:57:55 +0400

    Update EditionSVClist.cs

--- PrinterySVC/ImplementationsList/EditionSVClist.cs
+++ PrinterySVC/ImplementationsList/EditionSVClist.cs
@@ -70,21 +70,6 @@ namespace PrinterySVC.ImplementationsList
             }
             throw new Exception("Элемент не найден");
         }
-        public void DelElement(int id)
-        {
-            Edition element = source.Editions.FirstOrDefault(rec => rec.Number == id);
-            if (element != null)
-            {
-                // удаяем записи по компонентам при удалении изделия
-                source.EditionMaterials.RemoveAll(rec => rec.EditionNamber == id);
-                source.Editions.Remove(element);
-            }
-            else
-            {
-                throw new Exception("Элемент не найден");
-            }
-        }
-
         public void AddElement(EdiitionViewModel model)
         {
             Edition element = source.Editions.FirstOrDefault(rec => rec.EditionName == model.EditionName);
