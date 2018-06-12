cd6364ae75b015c5a0c2ed9589782beb31597148
Анастасия Мошина <moshinaan@gmail.com>
2018-06-13 03:49:24 +0400

    Weblab12

---
+++ PrinterySVC/Inteface/IReportSVC.cs
@@ -0,0 +1,19 @@
+﻿using PrinterySVC.BindingModel;
+using PrinterySVC.ViewModel;
+using System.Collections.Generic;
+
+namespace PrinterySVC.Inteface
+{
+    public interface IReportSVC
+    {
+        void SaveProductPrice(ReportBindingModel model);
+
+        List<RacksLoadViewModel> GetRacksLoad();
+
+        void SaveRacksLoad(ReportBindingModel model);
+
+        List<CustomerBookingsModel> GetCustomerOrders(ReportBindingModel model);
+
+        void SaveCustomerOrders(ReportBindingModel model);
+    }
+}
