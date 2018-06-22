b38b291fee9f29268266b7005bf8beede092a3a0
Анастасия Мошина <moshinaan@gmail.com>
2018-06-22 11:46:59 +0400

    Printlab11

--- PrinteryModel/Edition.cs
+++ PrinteryModel/Edition.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PrinteryModel
+namespace AbstractPrinteryModel
 {
     public class Edition
     {
+        [Key]
         public int Number { get; set; }
+
+        [Required]
         public string EditionName { get; set; }
+
+        [Required]
         public decimal CostEdition { get; set; }
-    }
+
+        [ForeignKey("EditionNumber")]
+        public virtual List<Booking> Bookings { get; set; }
+
+        [ForeignKey("EditionNamber")]
+        public virtual List<EditionMaterial> EditionMaterials { get; set; }
+    
+}
 }
