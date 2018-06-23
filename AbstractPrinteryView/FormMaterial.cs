c649429cc3fd308cf4ed7bd94ecc7c53495d0301
A.Moshina <31998009+ISE-21-ANASTASIA-MOSHINA@users.noreply.github.com>
2018-06-23 12:17:58 +0400

    Update FormMaterial.cs

--- AbstractPrinteryView/FormMaterial.cs
+++ AbstractPrinteryView/FormMaterial.cs
@@ -62,36 +62,43 @@ namespace AbstractPrinteryView
         private void buttonSave_Click_1(object sender, EventArgs e)
         {
 
-            if (string.IsNullOrEmpty(textBoxName.Text))
+           if (string.IsNullOrEmpty(textBoxName.Text))
             {
                 MessageBox.Show("Заполните название", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            try
+            string name = textBoxName.Text;
+            Task task;
+            if (id.HasValue)
             {
-                if (id.HasValue)
+                task = Task.Run(() => APIClient.PostRequestData("api/Material/UpdElement", new MaterialBindingModel
                 {
-                    service.UpElement(new MaterialBindingModel
-                    {
-                        Number = id.Value,
-                        MaterialName = textBoxName.Text
-                    });
-                }
-                else
-                {
-                    service.AddElement(new MaterialBindingModel
-                    {
-                        MaterialName = textBoxName.Text
-                    });
-                }
-                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                DialogResult = DialogResult.OK;
-                Close();
+                    Number = id.Value,
+                    MaterialName = name
+                }));
             }
-            catch (Exception ex)
+            else
             {
+                task = Task.Run(() => APIClient.PostRequestData("api/Material/AddElement", new MaterialBindingModel
+                {
+                    MaterialName = name
+                }));
+            }
+
+            task.ContinueWith((prevTask) => MessageBox.Show("Сохранение прошло успешно. Обновите список", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information),
+                TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.ContinueWith((prevTask) =>
+            {
+                var ex = (Exception)prevTask.Exception;
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                }
                 MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            Close();
+
         }
     }
 }
